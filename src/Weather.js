0b5a06e1e225d10f17149a7f86bd1a0b6cfc1ee7
libbwith2bs <100886874+libbwith2bs@users.noreply.github.com>
2022-08-22 14:10:12 -0500

    weather app content

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,45 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
-  return <div>Hello from weather</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Search for city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="partly cloudy"
+          />{" "}
+          80°F
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation</li>
+            <li>Humidity</li>
+            <li>Wind</li>
+          </ul>
+        </div>
+      </div>
+      Hello from weather
+    </div>
+  );
 }
