84ab6efcb4d97e0f9fc2f11a0b3851b53b673512
libbwith2bs <100886874+libbwith2bs@users.noreply.github.com>
2022-08-22 13:12:16 -0500

    created Weather.js component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
         <h1>Weather App</h1>
-        <button className="btn btn-primary">hello</button>
+        <Weather />
+        <footer>
+          This app was coded by Elizabeth Tutewiler and is{" "}
+          <a
+            href="https://github.com/libbwith2bs/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
       </div>
     </div>
   );
