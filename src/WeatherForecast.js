c32eef4a147bb710554e275d064e99c718855007
libbwith2bs <100886874+libbwith2bs@users.noreply.github.com>
2022-08-26 08:17:54 -0500

    else statement

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,36 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
     console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  let apiKey = "dff86e5088e22e74d9afbdbb6438d4bd";
-  let lon = props.data.coord.lon;
-  let lat = props.data.coord.lat;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
-
-  axios.get(apiUrl).then(handleResponse);
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col-2">
-          <div className="WeatherForecast-weekday">Thursday</div>
-          <div>
-            <img
-              className="WeatherForecast-icon"
-              src={props.data.icon}
-              alt={props.data.description}
-            />
-          </div>
-          <div className="WeatherForecast-temps">
-            <span className="WeatherForecast-max"> 80 </span>|
-            <span className="WeatherForecast-min"> 60</span>
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col-2">
+            <div className="WeatherForecast-weekday">Thursday</div>
+            <div>
+              <img
+                className="WeatherForecast-icon"
+                src={props.data.icon}
+                alt={props.data.description}
+              />
+            </div>
+            <div className="WeatherForecast-temps">
+              <span className="WeatherForecast-max">
+                {" "}
+                {Math.round(forecast[0].temp.max)}°{" "}
+              </span>
+              |
+              <span className="WeatherForecast-min">
+                {" "}
+                {Math.round(forecast[0].temp.min)}°
+              </span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "dff86e5088e22e74d9afbdbb6438d4bd";
+    let lon = props.data.coord.lon;
+    let lat = props.data.coord.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return "loading";
+  }
 }
